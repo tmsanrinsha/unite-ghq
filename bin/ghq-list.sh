5aeab67acf0238a7abae65c7e355da01c16323b7
tmsanrinsha <sanrinsha.tm@gmail.com>
2017-05-18 00:31:30 +0900

    後でまとめてsedすることで効率化

--- bin/ghq-list.sh
+++ bin/ghq-list.sh
@@ -14,13 +14,13 @@ ghq_roots="$(git config --path --get-all ghq.root)"
 for dir in $(ghq list --full-path)
 do
     if [ -d "$dir/.git" ]; then
-        stdout="$stdout\n$(ls -dl --time-style=+%s "$dir/.git" | sed 's/.*\([0-9]\{10\}\)/\1/' | sed 's/\/.git//')"
+        stdout="$stdout\n$(ls -dl --time-style=+%s "$dir/.git")"
     else
         stdout="$stdout\n$(echo 0 "$dir")"
     fi
 done
 
-stdout="$(echo -e "$stdout" | sort -nr | sed 's/^[0-9]\+ //')"
+stdout="$(echo -e "$stdout" | sed -e 's/.*\([0-9]\{10\}\)/\1/' -e 's/\/.git//' | sort -nr | sed 's/^[0-9]\+ //')"
 if [ -z "$full_path" ]; then
     echo "$stdout" | sed "s@\(${ghq_roots//$'\n'/\\|}\)/@@"
 else
